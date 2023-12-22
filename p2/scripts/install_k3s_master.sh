33ee678cc381698e5eafdca714900b9146dfdd8d
Clement Beaurain <clementbeaurainpro@gmail.com>
2023-12-22 12:57:07 +0100

    p2 fonctionne avec nodePort au lieu d'ingress + il faut trouver comment changer l'html de base de nginx avec les confs file

--- p2/scripts/install_k3s_master.sh
+++ p2/scripts/install_k3s_master.sh
@@ -1,3 +1,7 @@
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --debug --node-ip 192.168.56.110 --token mysecret" sh -
+kubectl config view
+sleep 10
 
-kubectl apply -f /config/app1.yaml
+kubectl apply -f /confs/app1.yaml
+kubectl apply -f /confs/app2.yaml
+kubectl apply -f /confs/app3.yaml
