de830eac92758a825731f4c6c818a3934e673e03
Clement Beaurain <clementbeaurainpro@gmail.com>
2023-12-26 23:31:10 +0100

    host fonctionne selon app

--- p2/scripts/install_k3s_master.sh
+++ p2/scripts/install_k3s_master.sh
@@ -5,15 +5,20 @@ sleep 10
 # install ingress nginx controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/cloud/deploy.yaml
 
-sleep 10
+kubectl apply -f /confs/app1.yaml
+kubectl apply -f /confs/app2.yaml
+kubectl apply -f /confs/app3.yaml
+
 # wait for ingress controller to be ready
+sleep 10
+
+echo -e "\e[90mWaiting for ingress controller to be ready...\e[0m"
+
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=320s
 
-kubectl apply -f /confs/app1.yaml
-kubectl apply -f /confs/app2.yaml
-kubectl apply -f /confs/app3.yaml
+echo -e "\e[32mIngress controller ready!\e[0m"
 
 kubectl apply -f /confs/ingress.yaml
