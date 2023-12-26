ab92fd2e24f2254515a947662ca75be83a3a97cb
Clement Beaurain <clementbeaurainpro@gmail.com>
2023-12-26 22:19:04 +0100

    ingress et ingress controller fonctionnent

--- p2/scripts/install_k3s_master.sh
+++ p2/scripts/install_k3s_master.sh
@@ -2,6 +2,18 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --d
 kubectl config view
 sleep 10
 
+# install ingress nginx controller
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/cloud/deploy.yaml
+
+sleep 10
+# wait for ingress controller to be ready
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=320s
+
 kubectl apply -f /confs/app1.yaml
 kubectl apply -f /confs/app2.yaml
 kubectl apply -f /confs/app3.yaml
+
+kubectl apply -f /confs/ingress.yaml
